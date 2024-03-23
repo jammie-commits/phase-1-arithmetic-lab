e7aebc24d8b2c913b2a18a5e20c4a87f5627b40a
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-23 15:58:46 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 
-// Write your code here
+// // Write your code here
+//   const result = multiply(2, 31);  
+//   const randomNum = getRandomNumber();
+//   const remainder = calculateMod(16, 4);  
+//   const maxValue = findMax([15, 20, 8]);
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+//   }
+  
+//   function getRandomNumber() {
+//     return Math.floor(Math.random() * 100) + 1;
+//   }
+  
+//   function calculateMod(num3, num4) {
+//     return num3 % num4;
+//   }
+  
+//   function findMax(numbers) {
+//     return Math.max(...numbers);
+//   }
+  
+  
+  
+  
+//   console.log("multiply:", result);
+//   console.log("random:", randomNum);
+//   console.log("mod:", remainder);
+//   console.log("max:", maxValue);
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 100);
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+let numbers = [10, 15, 20, 5];
+let max = Math.max(...numbers);
